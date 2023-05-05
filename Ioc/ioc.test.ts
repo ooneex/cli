7e80f2a92776de015c80e8f73b53896013fb3842
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-05 09:46:46 +0300

    test(ioc): add some tests

--- Ioc/ioc.test.ts
+++ Ioc/ioc.test.ts
@@ -1,12 +1,22 @@
-import { assertEquals } from "@ooneex/testing/asserts.ts";
+import { assertEquals, assertInstanceOf } from "@ooneex/testing/asserts.ts";
 import { describe, it } from "@ooneex/testing/bdd.ts";
-import { container, get, registerConstant } from "./mod.ts";
+import { getOrNull } from "./get.ts";
+import { container, ContainerException, get, registerConstant } from "./mod.ts";
 
 describe("Ioc", () => {
   const Keys = {
     One: Symbol.for(crypto.randomUUID()),
     Two: Symbol.for(crypto.randomUUID()),
   };
+
+  it("check exception", () => {
+    try {
+      get("no key");
+    } catch (e) {
+      assertInstanceOf(e, ContainerException);
+      assertEquals(e.message, `Cannot find value with key "no key"`);
+    }
+  });
 
   it("register value", () => {
     registerConstant(Keys.One, "One");
@@ -16,5 +26,6 @@ describe("Ioc", () => {
 
   it("get value", () => {
     assertEquals(get<string>(Keys.One), "One");
+    assertEquals(getOrNull<string>("no key"), null);
   });
 });
